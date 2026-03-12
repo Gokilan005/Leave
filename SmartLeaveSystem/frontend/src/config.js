a617b97a548d0007b1f768c6a58ac81ae049de84
Gokilan <gokilanthangavel@gmail.com>
2026-03-12 12:48:10 +0530

    ttt

--- SmartLeaveSystem/frontend/src/config.js
+++ SmartLeaveSystem/frontend/src/config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5002';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://leave-2t9d.onrender.com';
 export default API_BASE_URL;
